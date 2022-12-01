03f8960a20f8547a0d847dffa8619e648740c7d0
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-12-02 05:13:22 +0700

    tidy up jwt helper

--- helpers/jwtHelper.js
+++ helpers/jwtHelper.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
+const SECRET_TOKEN = process.env.SECRET_TOKEN || 'secret';
 
 const jwtHelper = {
   sign: (payload, options) => {
-    const SECRET_TOKEN = process.env.SECRET_TOKEN;
     return jwt.sign(payload, SECRET_TOKEN, options);
   },
 
   verify: (token, options) => {
-    const SECRET_TOKEN = process.env.SECRET_TOKEN;
     return jwt.verify(token, SECRET_TOKEN, options);
   },
 };
