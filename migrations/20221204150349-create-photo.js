0cde183e3a3ab28333233841635a31d89fc96f9d
perdanaph <danangputro9@gmail.com>
2022-12-04 22:45:15 +0700

    feat: create migration table photo

---
+++ migrations/20221204150349-create-photo.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Photos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      caption: {
+        type: Sequelize.STRING,
+      },
+      poster_image_url: {
+        type: Sequelize.STRING,
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+
+    // add constraint foreign key UserId
+    await queryInterface.addConstraint('Photos', {
+      fields: ['UserId'],
+      type: 'foreign key',
+      name: 'fk_user',
+      references: {
+        table: 'Users',
+        field: 'id',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Photos');
+  },
+};
