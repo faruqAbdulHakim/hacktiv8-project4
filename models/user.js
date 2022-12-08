e3dbb66cb639fb86ecd2523cb67c0cc6fbeba0b4
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-12-08 19:38:43 +0700

    fix models type

--- models/user.js
+++ models/user.js
@@ -4,14 +4,7 @@ const bcryptHelper = require('../helpers/bcryptHelper');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
+    static associate(models) {}
   }
   User.init(
     {
